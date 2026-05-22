fixed bug affecting weighting of reconstruction loss

m3m4/wgan/model.py
@@ -259,7 +259,7 @@ def g_loss(fake_scores, fake_y, y, recon_weight = 0.1):
 
     mean_fake_y = fake_y.mean(dim=0)
 
-    recon_loss = ((y - mean_fake_y) ** 2).mean()
+    recon_loss = ((y - mean_fake_y) ** 2).sum(dim=1).mean()
 
     return (1 - recon_weight) * adv_loss + recon_weight * recon_loss
 

mnist/wgan/train.py
@@ -38,7 +38,7 @@ def make_image(x, y):
 
 
 def center_mse(fake, real, mask):
-    return ((fake - real).pow(2) * mask).sum() / mask.sum()
+    return ((fake - real).pow(2) * mask).sum(dim=[1, 2, 3]).mean()
 
 
 def save_samples(path, ema_generator, x, y, z_dim, device, n_z=3):