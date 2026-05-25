852d2d26485521a26803ccf941147dd578a79841
Andrew Zhang <andrewzhang@Andrews-MacBook-Pro.local>
2026-05-25 03:09:12 -0700

    fixed seeding in test batch

--- cifar10/wgan/data.py
+++ cifar10/wgan/data.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 import torch
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader, Dataset, random_split
 from torchvision.datasets import CIFAR10
 from torchvision.transforms import Compose, Normalize, ToTensor
 
@@ -11,6 +11,18 @@ MASK_SIZE = 16
 IMAGE_SIZE = 32
 
 
+def apply_masks(images, idy, idx):
+    b, _, h, w = images.shape
+    mask = torch.zeros(b, 1, h, w)
+    for k in range(b):
+        mask[k, :, idy[k]:idy[k] + MASK_SIZE, idx[k]:idx[k] + MASK_SIZE] = 1
+
+    masked = images * (1 - mask)
+    y = images
+    x = torch.cat([masked, mask], dim=1)
+    return x, y
+
+
 def make_masked_batch(batch):
     images = torch.stack([image for image, _ in batch])
     b, _, h, w = images.shape
@@ -18,15 +30,29 @@ def make_masked_batch(batch):
     idy = torch.randint(0, h - MASK_SIZE + 1, (b,))
     idx = torch.randint(0, w - MASK_SIZE + 1, (b,))
 
-    mask = torch.zeros(b, 1, h, w)
-    for k in range(b):
-        mask[k, :, idy[k]:idy[k] + MASK_SIZE, idx[k]:idx[k] + MASK_SIZE] = 1
+    return apply_masks(images, idy, idx)
 
-    masked = images * (1 - mask)
 
-    y = images
-    x = torch.cat([masked, mask], dim=1)
-    return x, y
+def make_fixed_masked_batch(batch):
+    images = torch.stack([image for image, _, _ in batch])
+    idy = torch.tensor([p_idy for _, p_idy, _ in batch])
+    idx = torch.tensor([p_idx for _, _, p_idx in batch])
+
+    return apply_masks(images, idy, idx)
+
+
+class FixedMaskDataset(Dataset):
+    def __init__(self, base, idy, idx):
+        self.base = base
+        self.idy = idy
+        self.idx = idx
+
+    def __len__(self):
+        return len(self.base)
+
+    def __getitem__(self, k):
+        image, _ = self.base[k]
+        return image, int(self.idy[k]), int(self.idx[k])
 
 
 def get_dataloaders(
@@ -54,6 +80,14 @@ def get_dataloaders(
         generator=generator,
     )
 
+    val_idy = torch.randint(0, IMAGE_SIZE - MASK_SIZE + 1, (len(val),), generator=generator)
+    val_idx = torch.randint(0, IMAGE_SIZE - MASK_SIZE + 1, (len(val),), generator=generator)
+    test_idy = torch.randint(0, IMAGE_SIZE - MASK_SIZE + 1, (len(test),), generator=generator)
+    test_idx = torch.randint(0, IMAGE_SIZE - MASK_SIZE + 1, (len(test),), generator=generator)
+
+    val = FixedMaskDataset(val, val_idy, val_idx)
+    test = FixedMaskDataset(test, test_idy, test_idx)
+
     train_loader = DataLoader(
         train,
         batch_size=batch_size,
@@ -61,7 +95,7 @@ def get_dataloaders(
         generator=generator,
         collate_fn=make_masked_batch,
     )
-    val_loader = DataLoader(val, batch_size=batch_size, collate_fn=make_masked_batch)
-    test_loader = DataLoader(test, batch_size=batch_size, collate_fn=make_masked_batch)
+    val_loader = DataLoader(val, batch_size=batch_size, collate_fn=make_fixed_masked_batch)
+    test_loader = DataLoader(test, batch_size=batch_size, collate_fn=make_fixed_masked_batch)
 
     return train_loader, val_loader, test_loader
